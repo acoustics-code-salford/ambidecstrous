44b8fa8c1d9ed4ed75e734b8ca1c1d63094717ef
Marc Green <marc1701@gmail.com>
2023-10-05 16:03:02 +0100

    Added unit tests

---
+++ ambidecstrous/utils.py
@@ -0,0 +1,17 @@
+import json
+import numpy as np
+
+
+# TODO: modify mapping format so all contained in same file and only name is
+# needed as key
+def load_mapping(mapping_file, name):
+    with open(mapping_file, 'r') as file:
+        mapping = json.load(file)[name]
+        channel_numbers = [int(key) for key in mapping.keys()]
+        theta = np.radians(
+            [float(x['azimuth']) for x in mapping.values()]
+        )
+        phi = np.radians(
+            [float(x['elevation']) for x in mapping.values()]
+        )
+        return [channel_numbers, theta, phi]
